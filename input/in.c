f94581daf5eb0d8b0a2465ece4f3a201b59fd919
Mohamed Thalib <h.mohamedthalib@gdatech.co.in>
2009-12-15 12:17:19 +0530

    Small minor modification in the in.c

--- input/in.c
+++ input/in.c
@@ -2,7 +2,7 @@
 #include <linux/input.h>
 #include <linux/init.h>
 
-#define KBD 0
+#define SW 0
 
 MODULE_LICENSE("GPL");
 
@@ -11,7 +11,7 @@ unsigned int state;
 
 void ex_timeout(unsigned long unused)
 {
-#if KBD
+#if SW
 	int i;
 
 	for(i=0; i<=4; i++){
