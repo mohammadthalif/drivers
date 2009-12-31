8438003a8091a225d408e9eb987f2e4ad3c0044e
Mohamed Thalib <h.mohamedthalib@gdatech.co.in>
2009-12-31 18:07:54 +0530

    Added paralled port demo module

---
+++ par/par.c
@@ -0,0 +1,9 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/cdev.h>
+
+MODULE_AUTHOR("Mohamed Thalib H <h.mohamedthalib@gdatech.co.in>");
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Parallel port Demo")
+
+
