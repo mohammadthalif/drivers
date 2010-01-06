6ee23b8356cb4a92f79d240e7a6f91abddd50138
Mohamed Thalib H <hmthalib@gmail.com>
2010-01-06 23:09:00 +0530

    Commited modifictaion done localy Signed-off-by: Mohamed Thalib H <hmthalib@gmail.com>

--- par/par.c
+++ par/par.c
@@ -6,51 +6,61 @@
 
 #include <asm/io.h>
 
+#define BASE 0x378
+#define NR_COUNT 1
+
 MODULE_AUTHOR("Mohamed Thalib H <h.mohamedthalib@gdatech.co.in>");
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Parallel port Demo");
 
 int par_major = 200;
 int par_minor;
-unsigned long base = 0x0378;
-unsigned int reg_count = 1;
+
+
+
 static struct cdev par_dev;
 
 
-ssize_t par_read(struct file *fp, char *buff, size_t len, loff_t *off)
+static ssize_t par_read(struct file *fp, char *buff, size_t len, loff_t *off)
 {
 	return 0;
 }
-ssize_t par_write(struct file *fp, const char *buff, size_t len, loff_t *off)
-{
-	return 0;
-}
-
-
-int par_ioctl(struct inode *inode, struct file *fp, unsigned int cmd, unsigned long arg)
+static ssize_t par_write(struct file *fp, const char *buff, size_t len, loff_t *off)
 {
 	return 0;
 }
 
-int par_open(struct inode *node, struct file *fp)
+
+static int par_ioctl(struct inode *inode, struct file *fp, unsigned int cmd, unsigned long arg)
 {
-	static unsigned char data = 0;
+	return 0;
+}
+
+static int par_open(struct inode *node, struct file *fp)
+{
+	static u8 data = 0;
+	u8 i;
 	u8 buff;
 
-	barrier();
-	outb(base); 
-	buff = inb(base);
-	rmb();
-
-	printk(KERN_INFO "par: device opened, data = 0x%x, base=0x%x, \
-buff=%d \n", data, (unsigned int) base, buff);
+	for(i=1; i<=0xff; i<<=1) { 
+		barrier();
+		outb(i,BASE); 
+		wmb();
+		buff = inb(BASE);
+		rmb();
+		ssleep(1);
+		printk(KERN_INFO "par: device opened, data = 0x%x, BASE=0x%x, \
+buff=%d \n", i, (unsigned int) BASE, buff);
 	
-	data ^= 0xff;
+		data ^= 0xff;
 
+		if(i == 0)
+			break;
+	}
 	return 0;
 }
 
-int par_release(struct inode *inode, struct file *fp)
+static int par_release(struct inode *inode, struct file *fp)
 {
 	printk(KERN_INFO "par: device closed\n");
 	return 0;
@@ -73,12 +83,8 @@ int __init par_init(void)
 
 	printk(KERN_INFO "par: parlell port demo\n");
 
-	if(!check_region(base, reg_count)){
-		printk(KERN_INFO "par: check_region failed\n");
-	}
-	
-	if(! request_region(base, reg_count, "par")) {
-		printk(KERN_INFO "par: error cant get IOmem addr 0x%1x\n", (unsigned int) base);
+	if(! request_region(BASE, NR_COUNT, "par")) {
+		printk(KERN_INFO "par: error cant get IOmem addr 0x%1x\n", (unsigned int) BASE);
 		return -ENODEV;
 	}
 
@@ -118,7 +124,7 @@ void __exit par_exit(void)
 	 printk(KERN_INFO "par: unregister_chrdev_region()\n");
 	 unregister_chrdev_region(dev, 1);
 
-	 release_region(base, reg_count);
+	 release_region(BASE, NR_COUNT);
 }
 
 
