02f9d67b04dad2a8ab226c5fd2c43d71d8248eb2
Mohamed Thalib H <hmthalib@gmail.com>
2010-01-10 18:52:30 +0530

    Now added plug and play support to the par module. Tested the code in hard ware it is working as expected.
    Signed-off-by: Mohamed Thalib H <hmthalib@gmail.com>

--- par/par.c
+++ par/par.c
@@ -3,11 +3,12 @@
 #include <linux/cdev.h>
 #include <linux/fs.h>
 #include <linux/ioport.h>
+#include <linux/pnp.h>
 
 #include <asm/io.h>
 
 #define BASE 0x378
-#define NR_COUNT 1
+#define NR_COUNT 3
 
 MODULE_AUTHOR("Mohamed Thalib H <h.mohamedthalib@gdatech.co.in>");
 MODULE_LICENSE("GPL");
@@ -76,15 +77,14 @@ static struct file_operations par_ops = {
 	.write = par_write
 };
 
-int __init par_init(void)
+static int par_register(void)
 {
-	u32 ret;
 	dev_t dev;
+	int ret;
 
-	printk(KERN_INFO "par: parlell port demo\n");
-
-	if(! request_region(BASE, NR_COUNT, "par")) {
-		printk(KERN_INFO "par: error cant get IOmem addr 0x%1x\n", (unsigned int) BASE);
+	if(! request_region((unsigned long) BASE, NR_COUNT, "par")) {
+		printk(KERN_INFO "par: error cant get IOmem addr 0x%1x\n", \
+		       (unsigned int) BASE);
 		return -ENODEV;
 	}
 
@@ -104,11 +104,84 @@ int __init par_init(void)
 		goto exit_1;
 	}
 
+	outb(0xFF,BASE); 
+	wmb();
+
+	printk(KERN_INFO "par: device registerd\n");
+
 	return 0;
 
-exit_1:
+  exit_1:
 	unregister_chrdev_region(dev, 1);
-exit_out:
+  exit_out:
+	return ret;
+	
+}
+
+#ifdef CONFIG_PNP
+
+static const struct pnp_device_id par_pnp_tbl[] = {
+	/* Standard LPT Printer Port */
+	{.id = "PNP0400", .driver_data = 0},
+	{ }
+};
+
+MODULE_DEVICE_TABLE(pnp, par_pnp_tbl);
+
+static int par_pnp_probe(struct pnp_dev *dev, const struct pnp_device_id *id)
+{
+	
+	unsigned long io_lo;
+
+	if (pnp_port_valid(dev, 0) && 
+	    !(pnp_port_flags(dev, 0) & IORESOURCE_DISABLED)) {
+		io_lo = pnp_port_start(dev, 0);
+	} else
+		return -EINVAL;
+
+	dev_info(&dev->dev, "reported by %s\n", dev->protocol->name);
+
+	return 0;
+}
+
+static void par_pnp_remove(struct pnp_dev *dev)
+{
+	/* nothing to do in remove */
+}
+
+/* we only need the pnp layer to activate the device, at least for now */
+static struct pnp_driver par_pnp_driver = {
+	.name		= "par",
+	.id_table	= par_pnp_tbl,
+	.probe		= par_pnp_probe,
+	.remove		= par_pnp_remove,
+};
+
+#endif /* CONFIG_PNP */
+
+
+
+
+int __init par_init(void)
+{
+	int ret;
+
+	printk(KERN_INFO "par: parlell port demo\n");
+
+#ifdef CONFIG_PNP
+	
+	ret = pnp_register_driver(&par_pnp_driver);
+	
+	if (!ret) {
+		printk(KERN_INFO "par: pnp device registered\n");
+	} else {
+		printk(KERN_INFO "par: pnp device registration failed\n");
+		return ret;
+	}
+	
+#endif	
+	ret = par_register();
+
 	return ret;
 }
 
@@ -116,15 +189,18 @@ exit_out:
 void __exit par_exit(void)
 {
 
-	 dev_t dev = MKDEV(par_major, par_minor);
+	dev_t dev = MKDEV(par_major, par_minor);
 
-	 printk(KERN_INFO "par: cdev_del()\n");
-	 cdev_del(&par_dev);
+	printk(KERN_INFO "par: cdev_del()\n");
+	cdev_del(&par_dev);
 
-	 printk(KERN_INFO "par: unregister_chrdev_region()\n");
-	 unregister_chrdev_region(dev, 1);
+	printk(KERN_INFO "par: unregister_chrdev_region()\n");
+	unregister_chrdev_region(dev, 1);
 
-	 release_region(BASE, NR_COUNT);
+	release_region(BASE, NR_COUNT);
+	 
+	pnp_unregister_driver(&par_pnp_driver);
+		
 }
 
 
